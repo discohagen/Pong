e98d1c2e8f87f381accfd9876d2b66ceb1694cc5
Discohagen <leonziegenhagen22@gmail.com>
2023-02-11 18:27:35 +0100

    complete Pong clone

---
+++ script.js
@@ -0,0 +1,52 @@
+import Ball from "./Ball.js";
+import Paddle from "./Paddle.js";
+
+const ball = new Ball(document.querySelector("#ball"));
+const playerPaddle = new Paddle(document.querySelector("#paddle-player"));
+const computerPaddle = new Paddle(document.querySelector("#paddle-computer"));
+const playerScoreElem = document.querySelector("#player-score");
+const computerScoreElem = document.querySelector("#computer-score");
+
+let lastTime;
+function update(time) {
+  if (lastTime) {
+    const delta = time - lastTime;
+    ball.update(delta, [playerPaddle.rect(), computerPaddle.rect()]);
+    computerPaddle.update(delta, ball.y);
+    const hue = parseFloat(
+      getComputedStyle(document.documentElement).getPropertyValue("--hue")
+    );
+
+    document.documentElement.style.setProperty("--hue", hue + delta * 0.01);
+
+    if (isLose()) {
+      handleLose();
+    }
+  }
+
+  lastTime = time;
+  window.requestAnimationFrame(update);
+}
+
+function isLose() {
+  const rect = ball.rect();
+  return rect.right >= window.innerWidth || rect.left <= 0;
+}
+
+function handleLose() {
+  const rect = ball.rect();
+  if (rect.right >= window.innerWidth) {
+    playerScoreElem.textContent = parseInt(playerScoreElem.textContent) + 1;
+  } else {
+    computerScoreElem.textContent = parseInt(computerScoreElem.textContent) + 1;
+  }
+
+  ball.reset();
+  computerPaddle.reset();
+}
+
+document.addEventListener("mousemove", (e) => {
+  playerPaddle.position = (e.y / window.innerHeight) * 100;
+});
+
+window.requestAnimationFrame(update);
